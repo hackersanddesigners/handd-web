d442fcde1cc3d4eb76b93fcd3a5c737cc9e9a4e5
SirLucian <lucianblankevoort@MacBook-Pro-van-admin.local>
2015-09-04 14:59:29 +0200

    added the other Hacker and Designer pages & improved on styling

--- js/handd.js
+++ js/handd.js
@@ -10,10 +10,48 @@ angular.module('handd', ['ngRoute', 'ui.bootstrap'])
 
 .service('Homepage', function(wikiUrl, $http) {
   var self = this;
-  this.fetch = function (callback) {
+  this.fetchContent = function (callback) {
     $http.get(wikiUrl+'?action=parse&page=Hackers_%26_Designers&format=json').then(function(res) {
       var obj = res.data;
       callback(obj.parse.text['*']);
+    });
+  };
+  this.fetchTitle = function (callback) {
+    $http.get(wikiUrl+'?action=parse&page=Hackers_%26_Designers&format=json').then(function(res) {
+      var obj = res.data;
+      callback(obj.parse.title);
+    });
+  };
+})
+
+.service('Meetups', function(wikiUrl, $http) {
+  var self = this;
+  this.fetchContent = function (callback) {
+    $http.get(wikiUrl+'?action=parse&page=HD-meet-ups&format=json').then(function(res) {
+      var obj = res.data;
+      callback(obj.parse.text['*']);
+    });
+  };
+  this.fetchTitle = function (callback) {
+    $http.get(wikiUrl+'?action=parse&page=HD-meet-ups&format=json').then(function(res) {
+      var obj = res.data;
+      callback(obj.parse.title);
+    });
+  };
+})
+
+.service('Summeracademy', function(wikiUrl, $http) {
+  var self = this;
+  this.fetchContent = function (callback) {
+    $http.get(wikiUrl+'?action=parse&page=Hackers_%26_Designers_Summer_Academy&format=json').then(function(res) {
+      var obj = res.data;
+      callback(obj.parse.text['*']);
+    });
+  };
+  this.fetchTitle = function (callback) {
+    $http.get(wikiUrl+'?action=parse&page=Hackers_%26_Designers_Summer_Academy&format=json').then(function(res) {
+      var obj = res.data;
+      callback(obj.parse.title);
     });
   };
 })
@@ -21,18 +59,60 @@ angular.module('handd', ['ngRoute', 'ui.bootstrap'])
 .config(function($routeProvider) {
   $routeProvider
   .when('/', {
-    controller:'HomepageController as homepage',
+    controller:'HomepageCtrl as homepage',
     templateUrl:'homepage.html',
     resolve: {
     }
   })
+  .when('/meetups', {
+    controller: 'MeetupsCtrl as homepage',
+    templateUrl:'homepage.html',
+    resolve: {
+
+    }
+  })
+  .when('/summeracademy', {
+    controller: 'SummerAcademyCtrl as homepage',
+    templateUrl:'homepage.html',
+    resolve: {
+
+    }
+  })
 })
 
-.controller('HomepageController', function(Homepage) {
+.controller('HomepageCtrl', function(Homepage) {
   var homepage = this;
-  Homepage.fetch(function(html) {
-    console.log(html);
-    homepage.html = html; 
+  Homepage.fetchContent(function(content) {
+    console.log(content);
+    homepage.content = content; 
+  });
+  Homepage.fetchTitle(function(title) {
+    console.log(title);
+    homepage.title = title; 
+  });
+})
+
+.controller('MeetupsCtrl', function(Meetups) {
+  var homepage = this;
+  Meetups.fetchContent(function(content) {
+    console.log(content);
+    homepage.content = content; 
+  });
+  Meetups.fetchTitle(function(title) {
+    console.log(title);
+    homepage.title = title; 
+  });
+})
+
+.controller('SummerAcademyCtrl', function(Summeracademy) {
+  var homepage = this;
+  Summeracademy.fetchContent(function(content) {
+    console.log(content);
+    homepage.content = content; 
+  });
+  Summeracademy.fetchTitle(function(title) {
+    console.log(title);
+    homepage.title = title; 
   });
 });
         
