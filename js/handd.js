04288de7de7ccdbe4c4bf529cea8f1199b537299
SirLucian <lucianblankevoort@MacBook-Pro-van-admin.local>
2015-09-04 20:25:54 +0200

    Further Design changes&improved functionality

--- js/handd.js
+++ js/handd.js
@@ -8,6 +8,52 @@ angular.module('handd', ['ngRoute', 'ui.bootstrap'])
   };
 })
 
+.directive('ngDraggable', function($document, $window){
+  function makeDraggable(scope, element, attr) {
+    var startX = 0;
+    var startY = 0;
+
+    // Start with a random pos
+    var x = Math.floor((Math.random() * 500) + 40);
+    var y = Math.floor((Math.random() * 360) + 40);
+
+    element.css({
+      position: 'absolute',
+      cursor: 'pointer',
+      top: y + 'px',
+      left: x + 'px'
+    });
+
+    element.on('mousedown', function(event) {
+      event.preventDefault();
+      
+      startX = event.pageX - x;
+      startY = event.pageY - y;
+      
+      $document.on('mousemove', mousemove);
+      $document.on('mouseup', mouseup);
+    });
+
+    function mousemove(event) {
+      y = event.pageY - startY;
+      x = event.pageX - startX;
+
+      element.css({
+        top: y + 'px',
+        left: x + 'px'
+      });
+    }
+
+    function mouseup() {
+      $document.unbind('mousemove', mousemove);
+      $document.unbind('mouseup', mouseup);
+    }
+  }
+  return {
+    link: makeDraggable
+  };
+})
+
 .service('Homepage', function(wikiUrl, $http) {
   var self = this;
   this.fetchContent = function (callback) {
@@ -79,6 +125,30 @@ angular.module('handd', ['ngRoute', 'ui.bootstrap'])
     }
   })
 })
+
+.controller('PhotosCtrl', ['$scope', function($scope){
+  $scope.photos = [
+    '0001.jpg',
+    '0002.jpg',
+    '0003.jpg',
+    '0004.jpg',
+    '0005.jpg',
+    '0006.jpg',
+    '0007.jpg',
+    '0008.jpg',
+    '0009.jpg',
+    '0010.jpg',
+    '0011.gif',
+    '0012.jpg',
+    '0013.jpg',
+    '0014.jpg',
+    '0015.jpg',
+    '0016.jpg',
+    '0017.jpg',
+    '0018.jpg',
+    '0019.jpg',
+    '0020.jpg'];
+}])
 
 .controller('HomepageCtrl', function(Homepage) {
   var homepage = this;
