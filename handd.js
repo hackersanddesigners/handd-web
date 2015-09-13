8ed7734df076868152a2df873ee3f7f84477d7a4
JBG <jamesbryangraves@gmail.com>
2015-09-13 11:29:31 +0200

    oops we broke the main site

--- handd.js
+++ handd.js
@@ -1,19 +1,47 @@
 angular.module('handd', ['ngRoute'])
 
 .value('wikiUrl', 'http://wiki.hackersanddesigners.nl/mediawiki/api.php')
-
 .filter('unsafe', function($sce) {
   return function(val) {
     return $sce.trustAsHtml(val);
   };
 })
 
+/*
+.service('Utils', function() {
+  var self = this;
+  this.cleanLinks = function(html) {
+    var el = angular.element(html)
+    var links = el.find('a');
+    console.log(links);
+    for(var i = 0; i < links.length; i++) {
+      var href  = angular.element(links[i]).attr('href');
+      if(href.indexOf('mediawiki') != -1) {
+        var params = href.split('?');
+        if(params.length > 1) {
+          var keyValues = params[1].split('&');
+          for(var j = 0; j < keyValues.length; j++) {
+            var keyValue = keyValues[j].split('=');
+            if(keyValue[0] == 'title') {
+              return '{{ keyValue[1] }}' 
+            }
+          }
+        }
+        return 'http://wiki.hackersanddesigners.nl' + href;
+      }
+      return href;
+    }
+  };
+})
+*/
+
 .service('Homepage', function(wikiUrl, $http) {
   var self = this;
-  this.fetch = function (callback) {
-    $http.get(wikiUrl+'?action=parse&page=HD-meet-ups&format=json').then(function(res) {
+  this.fetch = function(callback) {
+    $http.get(wikiUrl+'?action=parse&page=Hackers_%26_Designers&format=json').then(function(res) {
       var obj = res.data;
-      callback(obj.parse.text['*']);
+      html = obj.parse.text['*'];
+      callback(html);
     });
   };
 })
@@ -31,7 +59,7 @@ angular.module('handd', ['ngRoute'])
 .controller('HomepageController', function(Homepage) {
   var homepage = this;
   Homepage.fetch(function(html) {
-    console.log(html);
+    //Utils.cleanLinks(html); 
     homepage.html = html; 
   });
 });
