b3d161520a4892e5d0eae34f05b77074f21d27b4
JBG <jamesbryangraves@gmail.com>
2015-10-08 14:34:51 +0200

    change to image

--- handd.js
+++ handd.js
@@ -7,38 +7,10 @@ angular.module('handd', ['ngRoute'])
   };
 })
 
-/*
-.service('Utils', function() {
-  var self = this;
-  this.cleanLinks = function(html) {
-    var el = angular.element(html)
-    var links = el.find('a');
-    console.log(links);
-    for(var i = 0; i < links.length; i++) {
-      var href  = angular.element(links[i]).attr('href');
-      if(href.indexOf('mediawiki') != -1) {
-        var params = href.split('?');
-        if(params.length > 1) {
-          var keyValues = params[1].split('&');
-          for(var j = 0; j < keyValues.length; j++) {
-            var keyValue = keyValues[j].split('=');
-            if(keyValue[0] == 'title') {
-              return '{{ keyValue[1] }}' 
-            }
-          }
-        }
-        return 'http://wiki.hackersanddesigners.nl' + href;
-      }
-      return href;
-    }
-  };
-})
-*/
-
 .service('Homepage', function(wikiUrl, $http) {
   var self = this;
   this.fetch = function(callback) {
-    $http.get(wikiUrl+'?action=parse&page=Hackers_%26_Designers&format=json').then(function(res) {
+    $http.get(wikiUrl+'?action=parse&page=Jeremy_Bailey_ad1&format=json').then(function(res) {
       var obj = res.data;
       html = obj.parse.text['*'];
       callback(html);
@@ -59,7 +31,6 @@ angular.module('handd', ['ngRoute'])
 .controller('HomepageController', function(Homepage) {
   var homepage = this;
   Homepage.fetch(function(html) {
-    //Utils.cleanLinks(html); 
     homepage.html = html; 
   });
 });
