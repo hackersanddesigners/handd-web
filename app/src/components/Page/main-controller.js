1ce4f43217e26f899d705313ee03b4f9d28a922b
SirLucian <lucian.blankevoort@gmail.com>
2015-10-09 06:14:24 +0200

    working....

--- app/src/components/Page/main-controller.js
+++ app/src/components/Page/main-controller.js
@@ -2,8 +2,13 @@
  * Created by lucianblankevoort on 08/10/15.
  */
 myApp.controller('mainCtrl', ['Page', '$scope', function(Page, $scope) {
+    //URL http://server.com/index.html#/?page=pageId
+    // Route: /:pageId
+    //$routeParams ==> {pageId: '%27Awkward%27_Error'};
+    //$route.current.params;
+    var pageId = '%27Awkward%27_Error';
     $scope.homepage = this;
-    Page.fetch(function(html) {
+    Page.fetch(pageId, function(html) {
         //Utils.cleanLinks(html);
         //Utils.cleanImages(html);
         $scope.homepage.html = html;
